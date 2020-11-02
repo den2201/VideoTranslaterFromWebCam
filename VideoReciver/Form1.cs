b964ff9b8fb40b58805980b63c4839b6159f1b90
Denis Nikiforov <Denis.Nikiforov@icl-services.com>
2020-11-02 08:07:16 +0300

    Add project files.

---
+++ VideoReciver/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace VideoReciver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            var client = new UdpClient(port);
+
+            while(true)
+            {
+                var data = await client.ReceiveAsync();
+                using (var stream = new MemoryStream(data.Buffer))
+                {
+                    pictureBox1.Image = new Bitmap(stream);
+                }
+
+
+
+            }
+
+        }
+
+       
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            MessageBox.Show(string.Join("\n", host.AddressList.Where(i => i.AddressFamily == AddressFamily.InterNetwork)));
+        }
+
+        
+    }
+}
---
+++ VideoSender/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using AForge.Video.DirectShow;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace VideoSender
+{
+    class Program
+    {
+        static IPEndPoint endPoint;
+        static UdpClient udpClient = new UdpClient();
+
+        
+         
+        static void Main(string[] args)
+        {
+            var considerIP =  ConfigurationManager.AppSettings.Get("considerIP");
+            var considerPort = int.Parse(ConfigurationManager.AppSettings.Get("considerPort"));
+            endPoint = new IPEndPoint(IPAddress.Parse(considerIP), considerPort);
+            
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+            Console.ReadLine();
+        }
+
+        private static void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            var bitMap = new Bitmap(eventArgs.Frame, 800, 600);
+
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    bitMap.Save(ms, ImageFormat.Jpeg);
+                    var bytes = ms.ToArray();
+                    udpClient.Send(bytes, bytes.Length, endPoint);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
